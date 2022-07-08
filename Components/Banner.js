ed986b8aa11aa0b8482566104fe12fa6e2b39fbe
Hitkumar <hit98987@gmail.com>
2022-07-08 18:52:14 +0200

    Added Header, Banner, All-cards, Footer

--- Components/Banner.js
+++ Components/Banner.js
@@ -1,9 +1,25 @@
-import React from 'react'
+import React from "react";
+import Image from "next/image";
 
 const Banner = () => {
   return (
-    <div>Banner</div>
-  )
-}
+    <div className="relative h-[300px] sm:h-[400px] lg:h-[400px] xl:h-[520px] 2xl:h-[700px]">
+      <Image
+        src="https://links.papareact.com/0fm"
+        alt="banner image"
+        layout="fill"
+        objectFit="cover"
+        priority={true}
+      />
 
-export default Banner
+      <div className="absolute top-1/2 w-full text-center">
+        <p className="text-sm sm:text-lg">Not sure where to go? Perfect.</p>
+        <button className="text-purple-500 bg-white px-10 py-4 shadow-md rounded-full cursor-pointer font-bold my-3 hover:shadow-xl active:scale-90 transition duration-150">
+          I am flexible
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default Banner;
--- Components/Header.js
+++ Components/Header.js
@@ -1,23 +1,29 @@
 import React from "react";
 import Image from "next/image";
-import { SearchIcon, GlobeAltIcon, MenuIcon, UserCircleIcon } from "@heroicons/react/solid";
+import {
+  SearchIcon,
+  GlobeAltIcon,
+  MenuIcon,
+  UserCircleIcon,
+} from "@heroicons/react/solid";
 
 const Header = () => {
   return (
-    <header className="sticky top-0 z-50 grid grid-cols-3 bg-white shadow-md p-5 md:px-10">
+    <header className="sticky  top-0 z-50 grid grid-cols-3 bg-white shadow-md p-5 md:px-10">
       {/* left */}
       <div className="relative flex items-center h-10 cursor-pointer my-auto">
         <Image
           src="https://links.papareact.com/qd3"
-          alt=""
+          alt="lOGO"
           layout="fill"
           objectFit="contain"
           objectPosition="left"
+          priority={true}
         />
       </div>
 
       {/* middle -  Search */}
-      <div className="flex items-center md:border-2 rounded-full py-2 md:shadow-sm">
+      <div className="flex items-center md:border-2 rounded-full py-2 md:shadow-sm ">
         <input
           className="flex-grow pl-5 bg-transparent outline-none text-sm text-gray-600 placeholder-gray-400 "
           type="text"
@@ -27,13 +33,13 @@ const Header = () => {
       </div>
 
       {/* right   */}
-      <div className="flex space-x-4 items-center justify-end text-gray-500">
+      <div className="flex space-x-2 items-center justify-end text-gray-500 ">
         <p className="hidden md:inline cursor-pointer">Become a host</p>
-        <GlobeAltIcon className="h-6 cursor-pointer"/>
+        <GlobeAltIcon className="h-6 cursor-pointer" />
 
         <div className="flex items-center space-x-2 border-2 p-2 rounded-full">
-            <MenuIcon className="h-6"/>
-            <UserCircleIcon className="h-6"/>
+          <MenuIcon className="h-6" />
+          <UserCircleIcon className="h-6" />
         </div>
       </div>
     </header>
